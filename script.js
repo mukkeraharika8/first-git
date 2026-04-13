9db395010bd200df22a40e8b6811f27b662b1674
HarikaMukkera <mukkeraharika8@gmail.com>
2026-04-13 09:19:25 +0530

    added script

---
+++ script.js
@@ -0,0 +1,48 @@
+//console.log(-1);
+
+// var num1="harika";
+// console.log(num1);
+// console.log(typeof num1);
+
+// var num1=10;
+// var num2="10";
+// if(num1 == num2) console.log(-1);
+
+// console.log(2**3);
+
+// var num1 = 10;
+// var fc=0;
+// for(var i=1;i<=num1;i++){
+//     if(num1 % i==0){
+//         fc++;
+//     }
+// }
+// if(fc==2){
+//     console.log("Prime number");
+// }else{
+//     console.log("Not a prime number");
+// }
+
+//day3
+// const obj = {
+//     id : 1,
+//     name:"guest",
+//     myData:[20,30]
+// }
+// console.log(obj.loc);
+
+// obj.loc="hyd"
+
+// console.log(obj);
+
+const users=[
+    {
+        id:1,
+        name:"user",
+        loc:"hyd"
+
+    }
+]
+console.log(users[0].name);
+
+
